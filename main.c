6238fb67a6167c3ab2c43fc0f3cd2d4a62663683
Brandon / DanAurea <laochi.bc@gmail.com>
2015-12-28 19:33:46 +0100

    Add cursor for diamond map

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <ESDL.h>
 #include <math.h>
 
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
-#define TILE_H 48
-#define TILE_W 96
-#define N 11
+#define SCREEN_WIDTH 1280 /**< Largeur de la fenêtre */
+#define SCREEN_HEIGHT 720 /**< Hauteur de la fenêtre */
+#define TILE_H 48 /**< Hauteur d'une tile */
+#define TILE_W 96 /**< Largeur d'une tile */
+#define N 11 /**< Taille de la map */
 
 typedef enum{diamond, staggered, slide}type_Map;
 
@@ -61,13 +61,15 @@ int offsetY(){
  * @param x Pointeur sur l'indice y
  */
 void getIndexMap(type_Map tMap, int posX, int posY, int * x, int * y){
+	posX -= offsetX(tMap);
 
 	if(tMap == diamond){
-		posX -= offsetX(tMap) + (TILE_W / 2); // Diamond map
+		posX -= TILE_W / 2; // Diamond map
 		posY -= offsetY();
 
-		* x = floor( ( (float)posX/ (TILE_W/2) + (float)posY/(TILE_H/2)) / 2 );
-		* y = floor( ( (float)posY/ (TILE_H/2) - (float)posX/(TILE_W/2)) / 2 );
+		* x = floor( ( (float)posX/ (TILE_W/2) + (float)posY/(TILE_H/2)) / 2 );;
+		* y = floor( ( (float)posY/ (TILE_H/2) - (float)posX/(TILE_W/2)) / 2 );;
+
 	}
 
 }
@@ -113,16 +115,38 @@ void drawTile(t_context * context , type_Map tMap, int posX, int posY){
 
 /**
  * Affiche un curseur à la position pointée
- * @param mouseX Coordonnées X de la souris
- * @param mouseY Coordonnées Y de la souris
+ * @param context Contexte dans lequel dessiner
+ * @param idCursor Identifiant du curseur
+ * @param tMap Type de la map
+ * @param x Indice X de la map
+ * @param y Indice Y de la map
  */
-void showCursor(int mouseX, int mouseY){
-	
+void showCursor(t_context * context, int * idCursor, type_Map tMap, int x, int y){
+	int posX, posY;
+
+	posX = x * TILE_W;
+	posY = y * TILE_H;
+
+	toIso(tMap, &posX, &posY);
+
+	posX += offsetX(tMap);
+	posY += offsetY();
+
+	if(* idCursor == - 1){
+		SDL_newImage(context, NULL, "cursor.png", posX, posY); // Initialise le curseur si pas encore dessiné
+		* idCursor = context->nbImg - 1;
+	}else{
+		SDL_editImage(context, * idCursor, posX, posY); // Met à jour la position du curseur
+	}
 }
 
+
+
 int main(){
-	int x, y, mouseX, mouseY;
+	int x, y, mouseX, mouseY, idCursor = -1;
 	int posX = 0, posY = 0;
+	int tmpX = -1, tmpY = -1;
+	type_Map tMap = diamond;
 
 	SDL_initWindow(SCREEN_WIDTH, SCREEN_HEIGHT, 0, "Tactics Arena", "M_ICON.png", 1, "global.ttf", 20, 0);
 
@@ -135,20 +159,30 @@ int main(){
 				posX =  x * TILE_W;
 				posY =  y * TILE_H;
 
-				drawTile(ingame, diamond, posX, posY);
+				drawTile(ingame, tMap, posX, posY);
 
 			}
 	}
 	
 	SDL_generate(ingame);
+	
+	x = -1;
+	y = -1;
 
 	while(1){
 		mouseX = SDL_getmousex();
 		mouseY = SDL_getmousey();
 		
-		getIndexMap(diamond, mouseX, mouseY, &x, &y);
+		getIndexMap(tMap, mouseX, mouseY, &x, &y);
 
-		showCursor(mouseX, mouseY); // Affiche en surbillance la zone pointée
+		if(tmpX != x || tmpY != y){
+			if(x >= 0 && x < N && y >= 0 && y < N){
+				showCursor(ingame, &idCursor, tMap, x, y); // Affiche en surbillance la zone pointée
+				
+				tmpX = x;
+				tmpY = y;
+			}
+		}
 		
 		SDL_generate(ingame);
 
